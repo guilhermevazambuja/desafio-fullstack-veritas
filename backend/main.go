584399c38f7d0ccedc97421f9933996e392aa3c3
Gui <88752991+guilhermevazambuja@users.noreply.github.com>
2025-11-12 17:30:02 -0300

    chore: configure CORS middleware for frontend integration

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
+
+	// CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Endpoints
 	router.POST("/tasks", addTask)
 	router.GET("/tasks/:id", getTask)
 	router.GET("/tasks", getTasks)
@@ -11,8 +28,7 @@ func main() {
 	router.PATCH("/tasks/:id", updateTask)
 	router.DELETE("/tasks/:id", deleteTask)
 
-	err := router.Run("localhost:9090")
-	if err != nil {
-		return
+	if err := router.Run("localhost:9090"); err != nil {
+		panic(err)
 	}
 }
